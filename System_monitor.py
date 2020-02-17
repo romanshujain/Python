2e52993f0893003a78af26ed7846b05e7f20b3eb
romanshujain <50296563+romanshujain@users.noreply.github.com>
2020-02-17 14:17:52 +0530

    Monitor with slack

--- System_monitor.py
+++ System_monitor.py
@@ -1,73 +1,33 @@
 import psutil
+import os
+import requests
+import json
+web_hook_url = 'https://hooks.slack.com/services/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'
+slack_msg_memory = {'text':'System is not safe, Used Memoery is more than 80%'}
+slack_msg_disk = {'text':'System is not safe, Disk usage is more than 90%'}
+slack_msg_cpu = {'text':'System is not safe, cpu usage is more than 80%'}
+
+hostname = "8.8.8.8"   # IP for google.com
 
 Memory_Usage=psutil.virtual_memory()
-Cpu_persent=psutil.cpu_times_percent(interval=1)
+Cpu_percent=psutil.cpu_times_percent(interval=1)
 Load=psutil.getloadavg()
 Disk_Usage=psutil.disk_usage('/')
 
 print("Memory Usage of system is: " + str(Memory_Usage))
-print("CPU percent of system is: " + str(Cpu_persent))
+print("CPU percent of system is: " + str(Cpu_percent))
 print("Load on system is: " + str(Load))
 print("Disk Usage of system is: " + str(Disk_Usage))
+response = os.system("ping -c 5 " + hostname)
 
 if Memory_Usage.percent >= 80:
 	print("This is not safe, Used Memoery is more than 80%")
+	requests.post(web_hook_url,data=json.dumps(slack_msg_memory))
 
 if Disk_Usage.percent >= 90:
 	print("This is not safe, Disk usage is more than 90%")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Memory=psutil.virtual_memory()
-#Cpu=
-
-
-#with open("/proc/loadavg", "r") as f:
- #   print("Average Load: " + f.read().strip())
-
-
-
-#print("Memory Info: ")
-#with open("/proc/meminfo", "r") as f:
- #   lines = f.readlines()
-
-#print("     " + lines[0].strip())
-#print("     " + lines[1].strip())
-
-
-# let's print CPU information
-#print("="*40, "CPU Info", "="*40)
-# number of cores
-#print("Physical cores:", psutil.cpu_count(logical=False))
-#print("Total cores:", psutil.cpu_count(logical=True))
-# CPU frequencies
-#cpufreq = psutil.cpu_freq()
-#print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
-#print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
-#print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
-# CPU usage
-#print("CPU Usage Per Core:")
-#for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
-#    print(f"Core {i}: {percentage}%")
-#print(f"Total CPU Usage: {psutil.cpu_percent()}%")
+	requests.post(web_hook_url,data=json.dumps(slack_msg_disk))
+if Cpu_percent.idle <= 20:
+	print("This is not safe, cpu usage is more than 80%")
+	requests.post(web_hook_url,data=json.dumps(slack_msg_cpu))
+	
